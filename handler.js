80cc953bb1398e5c471c57aac51e1043a68c6afe
eduardo3g <eduardosbrasil10@gmail.com>
2021-09-22 11:49:05 -0300

    feat: implement chaining function calls

--- handler.js
+++ handler.js
@@ -1,3 +1,7 @@
-module.exports.hello = async (name) => {
-  return `hello ${name}`;
+module.exports.add = async ({ x, y }) => {
+  return x + y;
+};
+
+module.exports.double = async (n) => {
+  return n * 2;
 };
